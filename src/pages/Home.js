3731060409ace38be477e5e446bf17beaeb8c641
Godswill omuka <omukagodswil@gmail.com>
2025-07-18 00:43:09 +0300

    create responsive navbar with logo and dropdown menus

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function Home() {
+    return (
+        <div>
+        <Navbar />
+        <h1>Welcome to the Dynamic Freelance Hub</h1>
+        <p>Your one-stop solution for all freelance needs.</p>
+        </div>
+    );
+    }
+
+export default Home;
