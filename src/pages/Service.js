4a5bc3fa672648c5fe44b9f6e23113dc9a85cee2
Godswill omuka <omukagodswil@gmail.com>
2025-11-19 12:48:18 +0300

    Add Infographics Design service page with detailed content, 12+ online images, responsive layout, and sidebar integration

--- src/pages/Service.js
+++ src/pages/Service.js
@@ -16,6 +16,7 @@ import InvitationCardDesign from "./Services/InvitationCardDesign";
 import CertificateDesign from "./Services/CertificateDesign";
 import TShirtArtworkDesign from "./Services/TShirtArtworkDesign";
 import MagazineBookLayout from "./Services/MagazineBookLayout";
+import InfographicsDesign from "./Services/InfographicsDesign";
 import "./Service.css";
 /* --- Placeholder Component for Unbuilt Pages --- */
 function ComingSoon({ title }) {
@@ -164,6 +165,7 @@ const slugToComponent = {
   "certificate-design": CertificateDesign,
   [slugify("T-Shirt Artwork Design")]: TShirtArtworkDesign,
   "magazine-book-layout": MagazineBookLayout,
+  "infographics-design": InfographicsDesign,
   // add more overrides as needed:
 };
 
